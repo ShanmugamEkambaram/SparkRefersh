85196b6821295f23bc7b2d4e36503c7aba57dd70
Shanmugam <datateam1602@gmail.com>
2019-02-09 21:41:07 +0530

    Initial Commit

---
+++ src/main/scala/Sss.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.{SparkContext,SparkConf}
+
+object Sss {
+  def main(args: Array[String]): Unit = {
+    println("Shanmugam")
+/*    val a=List("SHan","Tdeam","Rock","Bottom")
+    for(list<-a){
+      println("List of Names::"+ list)
+    }
+    val newList=("Shanmugam":: a)
+    for(b<-newList){
+      println("NewList::" + b)
+    }*/
+    val Conf=new SparkConf().setAppName("WordCount").setMaster("local")
+val sparkContext=new SparkContext(Conf)
+val fileInput = sparkContext.textFile("src/resources/3_pig_demo.txt")
+val counts = fileInput.flatMap(line=>line.split(" ")).map(word=>(word,1)).reduceByKey(_+_)
+    counts.foreach(println)
+
+  }
+}
